50a9877ecf12c887dc475c11fb4d2f3cb0920fcc
AhmedRazaSiddiqui <Ahmedsiddiqui551@gmail.com>
2023-03-20 22:23:32 +0500

    add-database

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,18 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmAsyncConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'mysqldb',
-      entities: [],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRootAsync(TypeOrmAsyncConfig),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
